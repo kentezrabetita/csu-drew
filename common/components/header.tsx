138b4f30804146c3d50fd7a57f836f01e3cdaed2
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-01-08 11:27:44 +0800

    update mobile menu

--- common/components/header.tsx
+++ common/components/header.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { FaGripLines, FaDiscord, FaRedditAlien } from 'react-icons/fa';
 import { CgClose } from 'react-icons/cg';
+import { MdDarkMode } from 'react-icons/md';
+import { IoMdSettings } from 'react-icons/io';
+
+import Link from 'next/link';
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -46,39 +50,81 @@ const Header = () => {
             </div>
             <div className='flex flex-col h-screen'>
               <div className='flex flex-row justify-center w-full p-4 space-x-4 border-b'>
-                <a className='flex flex-row items-center p-2 px-8 space-x-2 text-sm font-bold bg-gray-100 border rounded-md'>
+                <Link
+                  target={'_blank'}
+                  href={'https://www.reddit.com/r/carsu'}
+                  className='flex flex-row items-center p-2 px-8 space-x-2 text-sm font-bold bg-gray-100 border rounded-md'
+                >
                   <FaRedditAlien />
                   <span>Reddit</span>
-                </a>
-                <a className='flex flex-row items-center p-2 px-8 space-x-2 text-sm font-bold bg-gray-100 border rounded-md'>
+                </Link>
+                <Link
+                  target={'_blank'}
+                  href={'https://discord.gg/gPc3myEGZV'}
+                  className='flex flex-row items-center p-2 px-8 space-x-2 text-sm font-bold bg-gray-100 border rounded-md'
+                >
                   <FaDiscord />
                   <span>Discord</span>
-                </a>
+                </Link>
               </div>
               <div className='p-3 border-b'>
                 <ul className='text-sm font-bold'>
-                  <li className='p-2 bg-gray-100 border rounded-md'>Links</li>
-                  <li className='p-2'>Contacts</li>
-                  <li className='p-2'>About</li>
-                  <li className='p-2'>Contribute</li>
-                  <li className='p-2'>Changelog</li>
+                  <li className='p-2 bg-gray-100 border rounded-md'>
+                    <Link href={'/'}>Links</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/contacts'}>Contacts</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/about'}>About</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/contribute'}>Contribute</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/changelog'}>Changelog</Link>
+                  </li>
                 </ul>
               </div>
-              <div className='p-3 pb-20 border-b'>
+              <div className='p-3 pb-40 border-b'>
                 <ul className='text-sm font-bold'>
-                  <li className='p-2'>Services</li>
-                  <li className='p-2'>Courses</li>
-                  <li className='p-2'>Financial</li>
-                  <li className='p-2'>Social</li>
-                  <li className='p-2'>Housing</li>
-                  <li className='p-2'>Resources</li>
-                  <li className='p-2'>Miscellaneous</li>
-                  <li className='p-2'>Programs</li>
+                  <li className='p-2'>
+                    <Link href={'/services'}>Services</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/courses'}>Courses</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/financial'}>Financial</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/social'}>Social</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/housing'}>Housing</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/resources'}>Resources</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/miscellaneous'}>Miscellaneous</Link>
+                  </li>
+                  <li className='p-2'>
+                    <Link href={'/programs'}>Programs</Link>
+                  </li>
                 </ul>
               </div>
             </div>
-            <footer className='fixed bottom-0 left-0 w-full p-4 bg-blue-500'>
-              footer
+            <footer className='fixed bottom-0 left-0 z-10 w-full bg-white border-t'>
+              <div className='flex flex-row items-center justify-between p-2'>
+                <button className='flex flex-row items-center p-2 space-x-4 bg-gray-100 border rounded-md'>
+                  <MdDarkMode />
+                  <span>Dark Mode</span>
+                </button>
+                <a className='p-2'>
+                  <IoMdSettings />
+                </a>
+              </div>
             </footer>
           </div>
         </div>
