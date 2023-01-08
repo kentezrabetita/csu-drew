81d8741078e678cf1a942470937d4b1e85aa366a
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-01-08 11:28:13 +0800

    minor changes in main layout

--- common/content/home.tsx
+++ common/content/home.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const HomeContent = () => {
-  return <div>HomeContent</div>;
+  return (
+    <div className='container p-4'>
+      <h1 className='text-3xl font-bold'>Home</h1>
+    </div>
+  );
 };
 
 export default HomeContent;
--- common/layouts/main.tsx
+++ common/layouts/main.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import Sidebar from '../components/sidebar';
 import Header from '../components/header';
-import Footer from '../components/footer';
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
-      <Sidebar />
       <main>{children}</main>
-      <Footer />
     </>
   );
 };
