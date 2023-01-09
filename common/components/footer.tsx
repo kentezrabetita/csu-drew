12e1decaa2ab07d6550f79047a3d425d24003886
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-01-09 23:57:32 +0800

    add footer icons

--- common/components/footer.tsx
+++ common/components/footer.tsx
@@ -1,28 +1,65 @@
 import React from 'react';
 import Link from 'next/link';
+import {
+  FaGithub,
+  FaRegHeart,
+  FaPhone,
+  FaDiscord,
+  FaReddit
+} from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className='p-4 px-5 border-t'>
       <ul className='flex flex-col space-y-3 text-sm font-bold text-gray-500'>
         <li>
-          <Link href={'https://github.com/kentezrabetita'} target='_blank'>
-            Github
+          <Link
+            href={'https://github.com/kentezrabetita'}
+            className='flex items-center space-x-3'
+            target='_blank'
+          >
+            <FaGithub />
+            <span>Github</span>
           </Link>
         </li>
         <li>
-          <Link href={'https://github.com/kentezrabetita'} target='_blank'>
-            Contribute
+          <Link
+            href={'https://github.com/kentezrabetita'}
+            className='flex items-center space-x-3'
+            target='_blank'
+          >
+            <FaRegHeart />
+            <span>Contribute</span>
           </Link>
         </li>
         <li>
-          <Link href={'https://github.com/kentezrabetita'} target='_blank'>
-            Contact
+          <Link
+            href={'https://github.com/kentezrabetita'}
+            className='flex items-center space-x-3'
+            target='_blank'
+          >
+            <FaPhone />
+            <span>Contact</span>
           </Link>
         </li>
         <li>
-          <Link href={'https://github.com/kentezrabetita'} target='_blank'>
-            Discord
+          <Link
+            href={'https://github.com/kentezrabetita'}
+            className='flex items-center space-x-3'
+            target='_blank'
+          >
+            <FaDiscord />
+            <span>Discord</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={'https://github.com/kentezrabetita'}
+            className='flex items-center space-x-3'
+            target='_blank'
+          >
+            <FaReddit />
+            <span>Reddit</span>
           </Link>
         </li>
       </ul>
