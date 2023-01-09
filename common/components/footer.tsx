2bddccf4ec53f57d87e5077bfd0d80133e9f645f
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-01-09 23:49:56 +0800

    add footer section

--- common/components/footer.tsx
+++ common/components/footer.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className='p-4 px-5 border-t'>
+      <ul className='flex flex-col space-y-3 text-sm font-bold text-gray-500'>
+        <li>
+          <Link href={'https://github.com/kentezrabetita'} target='_blank'>
+            Github
+          </Link>
+        </li>
+        <li>
+          <Link href={'https://github.com/kentezrabetita'} target='_blank'>
+            Contribute
+          </Link>
+        </li>
+        <li>
+          <Link href={'https://github.com/kentezrabetita'} target='_blank'>
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link href={'https://github.com/kentezrabetita'} target='_blank'>
+            Discord
+          </Link>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
