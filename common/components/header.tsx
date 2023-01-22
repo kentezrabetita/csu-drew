0ae0725f3e68924afdedc4d6ff24feb844c12389
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-01-22 22:58:50 +0800

    add dark mode to hidden navbar

--- common/components/header.tsx
+++ common/components/header.tsx
@@ -40,28 +40,28 @@ const Header = ({ handleShowContent }: { handleShowContent: Function }) => {
 
       {/* MOBILE HIDDEN MENU */}
       {showMenu && (
-        <header className='container mx-auto overflow-y-hidden bg-white'>
+        <header className='container mx-auto overflow-y-hidden bg-white h-screen dark:bg-[#010101]'>
           <div className='w-full overflow-hidden bg-white '>
-            <div className='flex flex-row justify-between p-4 px-5 border-b'>
+            <div className='flex flex-row justify-between p-4 px-5 dark:border-gray-800 dark:bg-[#010101] dark:text-gray-500 border-b'>
               <div className='flex items-center space-x-3'>
                 <button
                   onClick={() => {
                     handleShowContent(showMenu);
                     setShowMenu(!showMenu);
                   }}
-                  className='p-2 border rounded-md'
+                  className='p-2 border rounded-md dark:border-gray-800'
                 >
                   <CgClose />
                 </button>
-                <span className='text-lg font-bold'>Drew</span>
+                <span className='text-lg font-bold dark:text-white'>Drew</span>
               </div>
             </div>
-            <div className='flex flex-col'>
-              <div className='flex flex-row justify-center w-full p-4 space-x-4 border-b'>
+            <div className='flex flex-col dark:border-gray-800 dark:bg-[#010101] dark:text-gray-500'>
+              <div className='flex flex-row justify-center w-full p-4 space-x-4 border-b dark:border-gray-800'>
                 <Link
                   target={'_blank'}
                   href={'https://www.reddit.com/r/carsu'}
-                  className='flex flex-row items-center p-2 px-8 space-x-2 text-sm font-bold bg-gray-100 border rounded-md'
+                  className='flex flex-row items-center p-2 px-8 space-x-2 text-sm font-bold bg-gray-100 border rounded-md dark:border-gray-800 dark:bg-gray-900 dark:text-gray-500'
                 >
                   <FaRedditAlien />
                   <span>Reddit</span>
@@ -69,7 +69,7 @@ const Header = ({ handleShowContent }: { handleShowContent: Function }) => {
                 <Link
                   target={'_blank'}
                   href={'https://discord.gg/gPc3myEGZV'}
-                  className='flex flex-row items-center p-2 px-8 space-x-2 text-sm font-bold bg-gray-100 border rounded-md'
+                  className='flex flex-row items-center p-2 px-8 space-x-2 text-sm font-bold bg-gray-100 border rounded-md dark:border-gray-800 dark:bg-gray-900 dark:text-gray-500'
                 >
                   <FaDiscord />
                   <span>Discord</span>
@@ -77,7 +77,7 @@ const Header = ({ handleShowContent }: { handleShowContent: Function }) => {
               </div>
               <div className='p-3'>
                 <ul className='text-sm font-bold'>
-                  <li className='p-2 bg-gray-100 border rounded-md'>
+                  <li className='p-2 bg-gray-100 border rounded-md dark:border-gray-800 dark:bg-gray-900 dark:text-gray-500'>
                     <Link href={'/'}>Links</Link>
                   </li>
                   <li className='p-2'>
@@ -89,9 +89,9 @@ const Header = ({ handleShowContent }: { handleShowContent: Function }) => {
                 </ul>
               </div>
             </div>
-            <footer className='fixed bottom-0 left-0 z-10 w-full bg-white border-t'>
+            <footer className='fixed bottom-0 left-0 z-10 w-full dark:border-gray-800 dark:bg-[#010101] dark:text-gray-500 border-t'>
               <div className='flex flex-row items-center justify-between p-2'>
-                <button className='flex flex-row items-center p-2 space-x-4 bg-gray-100 border rounded-md'>
+                <button className='flex flex-row items-center p-2 space-x-4 bg-gray-100 border rounded-md dark:bg-gray-900 dark:border-gray-800 dark:text-gray-400'>
                   <MdDarkMode />
                   <span>Dark Mode</span>
                 </button>
