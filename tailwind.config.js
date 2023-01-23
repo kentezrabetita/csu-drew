24d324ad2340a1c279e8c86ad8185f9b515ed596
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-01-23 23:09:53 +0800

    wrap app to next-theme provider

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'media',
+  darkMode: 'class',
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './common/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {}
