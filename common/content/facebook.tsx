b47e2245ca565283a7c166e6f655776783d5c3f9
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-01-25 05:49:53 +0800

    add facebook links json and display it to page

--- common/content/facebook.tsx
+++ common/content/facebook.tsx
@@ -1,6 +1,8 @@
 import Sidebar from '../components/sidebar';
 import Link from 'next/link';
 
+import FacebookLinks from '../constants/facebook-pages.json';
+
 import { FaExternalLinkSquareAlt } from 'react-icons/fa';
 
 const FacebookContent = () => {
@@ -9,24 +11,32 @@ const FacebookContent = () => {
       <Sidebar />
       <div className='flex flex-col items-center justify-center w-full md:ml-80'>
         <div className='container flex flex-col w-full p-4 px-5 pb-10 space-y-10 lg:px-20 xl:px-36'>
-          {/* Housing Section */}
-          <section>
-            <div className='flex flex-col space-y-3'>
-              <div className='flex items-center justify-between'>
-                <h3 className='text-xl font-bold'>Facebook Pages</h3>
-              </div>
-              <div className='grid grid-cols-2 gap-4 lg:grid-cols-3'>
-                <Link
-                  href={'/'}
-                  target='_blank'
-                  className='flex flex-row items-center p-4 space-x-4 text-sm font-bold text-gray-500 border border-gray-100 rounded-md dark:bg-gray-900 dark:text-gray-400 dark:border-gray-700 bg-gray-50'
-                >
-                  <FaExternalLinkSquareAlt />
-                  <span>Caraga State University</span>
-                </Link>
-              </div>
-            </div>
-          </section>
+          {FacebookLinks.map((link) => {
+            return (
+              <section key={link.category}>
+                <div className='flex flex-col space-y-3'>
+                  <div className='flex items-center justify-between'>
+                    <h3 className='text-xl font-bold'>{link.category}</h3>
+                  </div>
+                  <div className='grid grid-cols-2 gap-4 lg:grid-cols-3'>
+                    {link.pages.map((page) => {
+                      return (
+                        <Link
+                          key={page.name}
+                          href={page.link}
+                          target='_blank'
+                          className='flex flex-row items-center p-4 space-x-4 text-sm font-bold text-gray-500 border border-gray-100 rounded-md dark:bg-gray-900 dark:text-gray-400 dark:border-gray-700 bg-gray-50'
+                        >
+                          <FaExternalLinkSquareAlt />
+                          <span>{page.name}</span>
+                        </Link>
+                      );
+                    })}
+                  </div>
+                </div>
+              </section>
+            );
+          })}
         </div>
         {/* Footer Section */}
         <footer className='w-full border-t dark:border-gray-900'>
