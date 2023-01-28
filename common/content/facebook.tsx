05d44da283631fb0618a52ccf0892b9cd9ea8cbe
Kent Ezra Betita <kentezrabetita@gmail.com>
2023-01-28 13:33:10 +0800

    refactor: put footer to independent component

--- common/content/facebook.tsx
+++ common/content/facebook.tsx
@@ -2,6 +2,7 @@ import Sidebar from '../components/web/sidebar.web';
 import Link from 'next/link';
 import FacebookLinks from '../data/facebook-pages.json';
 import { FaExternalLinkSquareAlt } from 'react-icons/fa';
+import Footer from '../components/shared/footer.shared';
 
 const FacebookContent = () => {
   return (
@@ -44,22 +45,7 @@ const FacebookContent = () => {
           })}
         </div>
         {/* Footer Section */}
-        <footer className='w-full border-t dark:border-gray-900'>
-          <div className='container mx-auto'>
-            <div className='flex flex-row items-center justify-center p-3 space-x-4 text-gray-500'>
-              <p className='flex flex-row items-center p-1 px-2 space-x-4 bg-gray-100 rounded-md dark:text-gray-400 dark:border-gray-900 dark:bg-gray-900'>
-                Made with 💙 by
-                <Link
-                  href={'https://kentezrabetita.vercel.app'}
-                  target='_blank'
-                  className='font-semibold dark:text-blue-500'
-                >
-                  &nbsp;Kent Ezra Betita
-                </Link>
-              </p>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </div>
   );
--- common/content/home.tsx
+++ common/content/home.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Link from 'next/link';
 import Sidebar from '../components/web/sidebar.web';
 import Links from '../data/home-content.json';
 import LinkComponent from '../components/link';
+import Footer from '../components/shared/footer.shared';
 
 const HomeContent = () => {
   return (
@@ -38,22 +38,7 @@ const HomeContent = () => {
         </div>
 
         {/* Footer Section */}
-        <footer className='w-full border-t dark:border-gray-900'>
-          <div className='container mx-auto'>
-            <div className='flex flex-row items-center justify-center p-3 space-x-4 text-gray-500'>
-              <p className='flex flex-row items-center p-1 px-2 space-x-4 bg-gray-100 rounded-md dark:text-gray-400 dark:border-gray-900 dark:bg-gray-900'>
-                Made with 💙 by
-                <Link
-                  href={'https://kentezrabetita.vercel.app'}
-                  target='_blank'
-                  className='font-semibold dark:text-blue-500'
-                >
-                  &nbsp;Kent Ezra Betita
-                </Link>
-              </p>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </div>
   );
